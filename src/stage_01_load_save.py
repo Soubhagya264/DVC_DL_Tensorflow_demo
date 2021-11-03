d1f6d7aee90fc8bdb0c19bb711ccbac6e5bafed7
Soubhagya264 <71813414+Soubhagya264@users.noreply.github.com>
2021-11-03 11:47:24 +0530

    stage1 data added

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -2,23 +2,39 @@ from utils.all_utils import read_yaml,create_directory
 import argparse
 import pandas as pd
 import os
+import shutil
+from tqdm import tqdm
+
+def copy_file(source_path,local_dir):
+    lis_of_file-os.listdir(source_path)
+    n=len(lis_of_file)
+    for file in tqdm(lis_of_files,total=n,desc="copying file from {source_path} to {local_dir}",colour="green"):
+        src=os.path.join(source_path,file)
+        des=os.path.join(local_dir,file)
+        shutil.copy(src,des)
+
 
 def get_data(config_path):
     config=read_yaml(config_path)
     
-    remote_data_path=config["data_source"]
-    df=pd.read_csv(remote_data_path,sep=";")
+    source_download_path=config["source_download_path"]
+    local_data_dirs=config["local_data_dirs"]
+    
+    for source_path,local_dir in tqdm(zip(source_download_path,local_data_dirs),total=3,desc="list of folders",colour="red"):
+        create_directory([local_dir])
+        copy_file(source_path,local_dir)
+    
     # print(df.head())
     
-    artifacts_dir=config["artifacts"]["artifacts_dir"]
-    raw_local_dir=config["artifacts"]["raw_local_dir"]
-    raw_local_file=config["artifacts"]["raw_local_file"]
+    # artifacts_dir=config["artifacts"]["artifacts_dir"]
+    # raw_local_dir=config["artifacts"]["raw_local_dir"]
+    # raw_local_file=config["artifacts"]["raw_local_file"]
     
-    raw_local_dir_path=os.path.join(artifacts_dir,raw_local_dir)
-    create_directory(dirs=[raw_local_dir_path])
-    raw_local_file_path=os.path.join(raw_local_dir_path,raw_local_file)
+    # raw_local_dir_path=os.path.join(artifacts_dir,raw_local_dir)
+    # create_directory(dirs=[raw_local_dir_path])
+    # raw_local_file_path=os.path.join(raw_local_dir_path,raw_local_file)
     
-    df.to_csv(raw_local_file_path,sep=",",index=False)
+    # df.to_csv(raw_local_file_path,sep=",",index=False)
     
 if __name__=="__main__":
     args=argparse.ArgumentParser()
