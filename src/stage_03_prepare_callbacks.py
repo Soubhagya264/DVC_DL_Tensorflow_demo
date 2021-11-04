0112c2028ccb89472ab664bf1302c2e350cf294f
Soubhagya264 <71813414+Soubhagya264@users.noreply.github.com>
2021-11-04 11:59:16 +0530

    Callbacks fun added

--- src/stage_03_prepare_callbacks.py
+++ src/stage_03_prepare_callbacks.py
@@ -1,4 +1,5 @@
 from utils.all_utils import read_yaml,create_directory
+from utils.callbacks import create_and_save_tensorboard_callback,create_and_save_checkpoint_callback
 import argparse
 import pandas as pd
 import os
@@ -12,8 +13,24 @@ logging.basicConfig(filename=os.path.join(log_dir,"running_logs.log"),level=logg
 
 
 def prepare_callbacks(config_path,params_path):
-    pass
-
+    config=read_yaml(config_path)
+    params=read_yaml(params_path)
+    
+    artifacts=config["artifacts"]
+    artifacts_dir=artifacts["ARTIFACTS_DIR"]
+    tensorboard_log_dir=os.path.join(artifacts_dir,artifacts["TENSORBOARD_ROOT_LOG_DIR"])
+    checkpoint_dir=os.path.join(artifacts_dir,artifacts["CHECKPOINT_DIR"])
+    Callbacks_dir=os.path.join(artifacts_dir,artifacts["CALLBACKS_DIR"])
+    
+    create_directory(
+        [tensorboard_log_dir,
+        checkpoint_dir,
+        Callbacks_dir]
+    )
+    
+    create_and_save_tensorboard_callback(Callbacks_dir,tensorboard_log_dir)
+        
+    create_and_save_checkpoint_callback(Callbacks_dir,tensorboard_log_dir)
 
 if __name__=="__main__":
     args=argparse.ArgumentParser()
---
+++ src/utils/callbacks.py
@@ -0,0 +1,8 @@
+
+
+
+def create_and_save_checkpoint_callback(Callbacks_dir,tensorboard_log_dir):
+    pass
+
+def create_and_save_tensorboard_callback(Callbacks_dir,tensorboard_log_dir):
+    pass
