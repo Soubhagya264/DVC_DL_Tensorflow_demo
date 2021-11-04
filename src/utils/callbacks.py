e76b9d4d122c4b5871dc2d0857f4a1ce671cb75d
Soubhagya264 <71813414+Soubhagya264@users.noreply.github.com>
2021-11-04 12:43:10 +0530

    checkpoint fun done

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -7,8 +7,17 @@ from utils.all_utils import get_time_stamp
 
 
 
-def create_and_save_checkpoint_callback(Callbacks_dir,tensorboard_log_dir):
-    pass
+def create_and_save_checkpoint_callback(callbacks_dir,checkpoint_dir):
+    checkpoint_file_path = os.path.join(checkpoint_dir, "ckpt_model.h5")
+    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_file_path,
+        save_best_only=True
+    )
+
+    ckpt_callback_filepath = os.path.join(callbacks_dir, "checkpoint_cb.cb")
+    joblib.dump(checkpoint_callback, ckpt_callback_filepath)
+    logging.info(f"tensorboard callback is being saved at {ckpt_callback_filepath}")
+
 def create_and_save_tensorboard_callback(Callbacks_dir,tensorboard_log_dir):
     unique_name=get_time_stamp("tb_logs")
     tb_running_log_dir=os.path.join(tensorboard_log_dir,unique_name)
